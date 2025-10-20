66cc72fb688c8f7ad15aca1a17ccc71be6df7b30
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-20 03:34:41 -0600

    fix: adecuamos todo el css y el js de forma correcta

--- todo-app/src/todos/app.js
+++ todo-app/src/todos/app.js
@@ -8,7 +8,7 @@ export const App =( elementId ) => {
 //cuando se llame la funcion app( ) se llama esta funcion anonima autoejecutable
 (()=> {
     const app = document.createElement('div');
-    app.innerHTML= '<h1>Hola Mundo</h1>';
+    app.innerHTML= html;
     document.querySelector(elementId).append(app);
 
 })();
