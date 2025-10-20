c72591a164552982f55ff589eeaa52491502c1c8
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-20 14:47:28 -0600

    feat:agregamos la libreria uuid para generar identificadores unicos dentro de la aplicación

--- todo-app/src/models/todo.model.js
+++ todo-app/src/models/todo.model.js
@@ -1,3 +1,5 @@
+import {v4 as uuid } from 'uuid';
+
 export class Todo {
 
     /**
@@ -5,7 +7,8 @@ export class Todo {
      * @param {String} description 
      */
     constructor( description ) {
-        this.id =1;
+        
+        this.id = uuid();//genera un id unico
         this.description = description;
         this.done = false;
         this.createdAt = new Date();
