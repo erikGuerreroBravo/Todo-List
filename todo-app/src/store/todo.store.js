1dcfa61bb246d5a61821113c6cb315d75ed6c097
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-20 13:45:25 -0600

    feat: se crea la clase todo.models para trabajar con los estados d ela aplicacion

---
+++ todo-app/src/store/todo.store.js
@@ -0,0 +1,17 @@
+import { Todo } from "../models/todo.model";
+
+const Filters ={
+    All: 'all',
+    Completed: 'completed',
+    Pending: 'pending'
+}
+
+const state ={
+    todos: [
+        new Todo('Aprender JavaScript'),
+        new Todo('Aprender Vue.js'),
+        new Todo('Aprender Vite'),
+    ],
+    filter: Filters.All,
+   
+}
