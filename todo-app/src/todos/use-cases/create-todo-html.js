40fabe0a923b0a8848b534dde3f298f20096a754
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-22 12:56:52 -0600

    fix:modificamos el componente dinamico de html con el elemento padre inicial.

--- todo-app/src/todos/use-cases/create-todo-html.js
+++ todo-app/src/todos/use-cases/create-todo-html.js
@@ -2,9 +2,9 @@ import { Todo } from '../../models/todo.model.js';
 
 /**
  * 
- * @param {Todo} todo 
+ * @param {Todo} todo : recibe una clase del tipo todo
  */
-export const createHtml = (todo) =>{
+export const createHtml = (todo) => {
 
     if(!todo) throw new Error('Todo is required');
 
