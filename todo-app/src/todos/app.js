bd494d715337ba87eb766dce6250ef6f87ba6648
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2025-10-26 02:57:42 -0600

    fix: se crea el evento para eliminar el id de la lista de forma adecuada.

--- todo-app/src/todos/app.js
+++ todo-app/src/todos/app.js
@@ -46,5 +46,12 @@ todoList.addEventListener('click', ( event )=>{
     displayTodos();
 });
 
+todoList.addEventListener('click', ( event )=>{
+    const isDestroyElement = event.target.className==='destroy';
+    const element= event.target.closest('[ data-id]');
+     if(!element || !isDestroyElement) return;
+    todoStore.deleteTodo( element.getAttribute('data-id') );
+    displayTodos();
+});
 
 }
